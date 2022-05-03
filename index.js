dbdb5dbb4d4cdd57b600e557c51ef15d7dbbe122
Bruno Mayerle Leite <brleite@gmail.com>
2022-05-03 13:21:40 -0300

    Funcionalidade para permitir array de operações a serem realizadas em um site antes da comparação

--- index.js
+++ index.js
@@ -6,6 +6,179 @@ const diff_match_patch = require('diff-match-patch');
 const jsdiff = require('./utils/jsdiff');
 const diff = require('node-htmldiff');
 
+async function operacaoKeyboard(page, seletor, valor) {
+  utils.log('inicio operação keyboard');
+  utils.log(seletor);
+
+  await page.waitForSelector(seletor);
+  await page.focus(seletor);
+  utils.log("wait for selector")
+  await page.keyboard.type(valor)
+  utils.log("keyboard type")
+
+  utils.log('fim operação keyboard');
+}
+
+async function operacaoClick(page, seletor) {
+  utils.log('inicio operação click');
+  utils.log(seletor);
+
+  await page.waitForSelector(seletor);
+  utils.log("wait for selector")
+  await page.click(seletor);
+  utils.log("click selector")
+
+  utils.log('fim operação click');
+}
+
+async function operacaoClickEvaluate(page, seletor, delay) {
+  utils.log('inicio operação clickEvaluate');
+  utils.log(seletor);
+
+  await page.waitForSelector(seletor);
+  utils.log("wait for selector")
+
+  await page.evaluate(([seletor]) => {
+      document.querySelector(seletor).click();
+  }, [seletor]);
+
+  if (delay) {
+    await page.waitForTimeout(delay);
+  }
+
+  utils.log('fim operação clickEvaluate');
+}
+
+async function operacaoWait(page, seletor) {
+  utils.log('inicio operação wait');
+  utils.log(seletor);
+
+  const operacaoSeletor = await page.waitForSelector(seletor);
+  utils.log("wait for selector")
+
+  utils.log('fim operação wait');
+}
+
+async function operacaoCompare(page,
+                         url,
+                         notify,
+                         bot_chatIds,
+                         seletor,
+                         verificarSomenteAreaSelecionada,
+                         arquivoSiteAtualizado,
+                         arquivoSiteAntesAtualizacao,
+                         arquivoSiteAlteracoes,
+                         arquivoSiteAlteracoesScreenshot) {
+  utils.log('inicio operação compare');
+  utils.log(seletor);
+
+  const operacaoSeletor = await page.waitForSelector(seletor);
+  utils.log("wait for selector")
+
+  await compare(page,
+          url,
+          notify,
+          bot_chatIds,
+          arquivoSiteAtualizado,
+          arquivoSiteAntesAtualizacao,
+          arquivoSiteAlteracoesScreenshot,
+          arquivoSiteAlteracoes,
+          verificarSomenteAreaSelecionada,
+          seletor);
+
+  utils.log('fim operação compare');
+}
+
+async function compare(page,
+                 url,
+                 notify,
+                 bot_chatIds,
+                 arquivoSiteAtualizado,
+                 arquivoSiteAntesAtualizacao,
+                 arquivoSiteAlteracoesScreenshot,
+                 arquivoSiteAlteracoes,
+                 verificarSomenteAreaSelecionada,
+                 seletor) {
+  let novoSite;
+  if (verificarSomenteAreaSelecionada) {
+    const operacaoSeletor = await page.waitForSelector(seletor);
+
+    novoSite = await operacaoSeletor.evaluate(domElement => {
+      const novoSiteTmp = domElement.innerHTML;
+
+      return novoSiteTmp;
+    });
+  } else {
+    novoSite = await page.content();
+  }
+
+  var beautify_html = require('js-beautify').html;
+  novoSite = beautify_html(novoSite, { indent_size: 2 })
+
+  utils.log("Novo Site obtido");
+
+  if (fs.existsSync(arquivoSiteAtualizado)) {
+    const currentSite = fs.readFileSync(arquivoSiteAtualizado, 'utf8');
+
+    utils.log("Site atual obtido");
+
+    if (novoSite && currentSite && currentSite != novoSite) {
+      const msg = `Houve atualização no site ${url}`
+      utils.log(msg)
+
+      /* const diff = new diff_match_patch.diff_match_patch();
+      const diffs = diff.diff_main(currentSite, novoSite);
+      diff.diff_cleanupSemantic(diffs)
+      const alteracoes = diff.diff_prettyHtml(diffs); */
+      // const alteracoes = jsdiff.diffString(currentSite, novoSite);
+      const alteracoes = diff(currentSite, novoSite);
+
+      if (notify) {
+        utils.sendBotMessage(msg, bot_chatIds);
+      }
+
+      fs.writeFileSync(arquivoSiteAtualizado, novoSite);
+      fs.writeFileSync(arquivoSiteAntesAtualizacao, currentSite);
+
+      if (verificarSomenteAreaSelecionada === true) {
+        await page.evaluate(async ([seletor, newInnerHTML]) => {
+          let dom = document.querySelector(seletor);
+          dom.innerHTML = newInnerHTML;
+        }, [seletor, alteracoes]);
+      } else {
+        await page.setContent(alteracoes)
+      }
+
+      await page.addStyleTag({content: 'del {background-color: tomato;}'})
+      await page.addStyleTag({content: 'ins {background-color: lightgreen;}'})
+
+      await page.screenshot({
+        path: arquivoSiteAlteracoesScreenshot,
+        fullPage: true,
+        type: "jpeg",
+        quality: 100
+      });
+
+      siteAlteracoes = await page.content();
+      fs.writeFileSync(arquivoSiteAlteracoes, siteAlteracoes);
+
+      if (notify && arquivoSiteAlteracoes) {
+        utils.sendBotDocument(arquivoSiteAlteracoes, "HTML com as alterações", bot_chatIds);
+      }
+
+      if (notify && arquivoSiteAlteracoesScreenshot) {
+        utils.sendBotImage(arquivoSiteAlteracoesScreenshot, "Alterações", bot_chatIds);
+      }
+    } else {
+      const msg = `Não Houve atualização no site ${url}`
+      utils.log(msg)
+    }
+  } else {
+    utils.log(`Arquivo inexistente: ${arquivoSiteAtualizado}. Criando um arquivo inicial.`)
+    fs.writeFileSync(arquivoSiteAtualizado, novoSite);
+  }
+}
+
 (async () => {
   const browser = await puppeteer.launch({
     // executablePath: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
@@ -39,16 +212,15 @@ const diff = require('node-htmldiff');
 
   try {
     for (let p of urls) {
-    // urls.forEach(async p => {
       utils.log(`URL: ${p.url}`);
       utils.log(`SELETOR: ${p.seletor}`);
-      utils.log(`ARQUIVO SITE ATUALIZADO: ${p.arquivoSiteAtualizado}`);
-      utils.log(`ARQUIVO SITE ANTES DA ATUALIZAÇÃO: ${p.arquivoSiteAntesAtualizacao}`);
-      utils.log(`ARQUIVO SITE ALTERAÇÕES: ${p.arquivoSiteAlteracoes}`);
-      utils.log(`ARQUIVO SITE ALTERAÇÔES SCREENSHOT: ${p.arquivoSiteAlteracoesScreenshot}`);
       utils.log(`DELAY: ${p.delay}`);
 
-      // await page.exposeFunction("getConfig", () => p);
+      if (!p.enabled) {
+        utils.log("Verficação desabilitada.");
+
+        continue;
+      }
 
       await Promise.all([
         page.goto(p.url),
@@ -61,164 +233,47 @@ const diff = require('node-htmldiff');
 
       utils.log("goto url");
       pageTmp = await page.content();
-      // console.log(pageTmp);
 
       let verificarSomenteAreaSelecionada = false;
       let seletorComparacao;
       let novoSite;
       if (p.operacoes && p.operacoes.length > 0) {
-        console.log('inicio operações')
+        utils.log('inicio operações')
         for (let operacao of p.operacoes) {
           if (operacao.tipo === 'click') {
-            console.log('inicio operação click');
-
-            const seletorClick = operacao.seletor;
-            console.log(seletorClick);
-
-            await page.waitForSelector(seletorClick);
-            console.log("wait for selector")
-            await page.click(seletorClick);
-            console.log("click selector")
-            // await page.waitForNavigation();
-            // console.log("wait for navigation")
-
-            console.log('fim operação click');
+            await operacaoClick(page, operacao.seletor);
+          } else if (operacao.tipo === 'clickEvaluate') {
+            await operacaoClickEvaluate(page, operacao.seletor, operacao.delay);
           } else if (operacao.tipo === 'wait') {
-            console.log('inicio operação wait');
-
-            const seletorWait = operacao.seletor;
-            console.log(seletorWait);
-
-            const operacaoSeletor = await page.waitForSelector(seletorWait);
-            console.log("wait for selector")
-
-            if (operacao.verificarSomenteAreaSelecionada) {
-              verificarSomenteAreaSelecionada = true;
-              seletorComparacao = seletorWait;
-              novoSite = await operacaoSeletor.evaluate(domElement => {
-                const novoSiteTmp = domElement.innerHTML;
-
-                return novoSiteTmp;
-              });
-            }
-
-            console.log('fim operação wait');
+            await operacaoWait(page, operacao.seletor);
+          } else if (operacao.tipo === 'compare') {
+            await operacaoCompare(page,
+                            p.url,
+                            config.notify,
+                            p.bot_chatIds,
+                            operacao.seletor,
+                            operacao.verificarSomenteAreaSelecionada,
+                            operacao.arquivoSiteAtualizado,
+                            operacao.arquivoSiteAntesAtualizacao,
+                            operacao.arquivoSiteAlteracoes,
+                            operacao.arquivoSiteAlteracoesScreenshot);
+          } else if (operacao.tipo === 'keyboard') {
+            await operacaoKeyboard(page, operacao.seletor, operacao.valor);
           }
         }
       } else {
-        const elementoSeletor = await page.waitForSelector(p.seletor);
-        utils.log("Seletor localizado");
-
-        if (p.verificarSomenteAreaSelecionada == true) {
-          utils.log("Verificando somente área selecionada");
-
-          verificarSomenteAreaSelecionada = true;
-          seletorComparacao = p.seletor;
-          console.log(seletorComparacao);
-          novoSite = await elementoSeletor.evaluate(domElement => {
-            const novoSiteTmp = domElement.innerHTML;
-
-            return novoSiteTmp;
-          });
-          // console.log(novoSite);
-        } else {
-          utils.log("Verificando página inteira");
-
-          verificarSomenteAreaSelecionada = false;
-          novoSite = await page.content();
-        }
-      }
-
-
-      var beautify_html = require('js-beautify').html;
-      novoSite = beautify_html(novoSite, { indent_size: 2 })
-
-      utils.log("Novo Site obtido");
-      // utils.log(`Novo Site: ${novoSite}`);
-
-      if (fs.existsSync(p.arquivoSiteAtualizado)) {
-        const currentSite = fs.readFileSync(p.arquivoSiteAtualizado, 'utf8');
-
-        utils.log("Site atual obtido");
-        /* utils.log("currentSite");
-        console.log(currentSite)
-        utils.log("novoSite");
-        utils.log(novoSite); */
-        // utils.log(`Current Site: ${currentSite}`)
-
-        if (novoSite && currentSite && currentSite != novoSite) {
-          const msg = `Houve atualização no site ${p.url}`
-          utils.log(msg)
-
-          /* const diff = new diff_match_patch.diff_match_patch();
-          const diffs = diff.diff_main(currentSite, novoSite);
-          diff.diff_cleanupSemantic(diffs)
-          const alteracoes = diff.diff_prettyHtml(diffs); */
-          // const alteracoes = jsdiff.diffString(currentSite, novoSite);
-          const alteracoes = diff(currentSite, novoSite);
-
-          // utils.log(alteracoes);
-
-          // await page.exposeFunction("getAlteracoes", () => alteracoes);
-          // utils.log(jsdiff.diffString(currentSite, novoSite));
-          // utils.log(jsdiff);
-
-          if (config.notify) {
-            utils.sendBotMessage(msg, p.bot_chatIds);
-          }
-
-          fs.writeFileSync(p.arquivoSiteAtualizado, novoSite);
-          fs.writeFileSync(p.arquivoSiteAntesAtualizacao, currentSite);
-
-          /* if (p.verificarSomenteAreaSelecionada == true) {
-            await page.evaluate(async () => {
-              let dom = document.querySelector((await getConfig()).seletor);
-              dom.innerHTML = await getAlteracoes();
-            });
-          } else {
-
-          } */
-          if (verificarSomenteAreaSelecionada === true) {
-            await page.evaluate(async ([seletor, newInnerHTML]) => {
-              let dom = document.querySelector(seletor);
-              dom.innerHTML = newInnerHTML;
-            }, [seletorComparacao, alteracoes]);
-          } else {
-            await page.setContent(alteracoes)
-          }
-
-          await page.addStyleTag({content: 'del {background-color: tomato;}'})
-          await page.addStyleTag({content: 'ins {background-color: lightgreen;}'})
-
-          await page.screenshot({
-            path: p.arquivoSiteAlteracoesScreenshot,
-            fullPage: true,
-            type: "jpeg",
-            quality: 100
-          });
-
-          siteAlteracoes = await page.content();
-          fs.writeFileSync(p.arquivoSiteAlteracoes, siteAlteracoes);
-
-          if (config.notify && p.arquivoSiteAlteracoes) {
-            utils.sendBotDocument(p.arquivoSiteAlteracoes, "HTML com as alterações", p.bot_chatIds);
-          }
-
-          if (config.notify && p.arquivoSiteAlteracoesScreenshot) {
-            utils.sendBotImage(p.arquivoSiteAlteracoesScreenshot, "Alterações", p.bot_chatIds);
-          }
-
-          // fs.writeFileSync(p.arquivoSiteAlteracoes, alteracoes);
-        } else {
-          const msg = `Não Houve atualização no site ${p.url}`
-          utils.log(msg)
-        }
-      } else {
-        utils.log(`Arquivo inexistente: ${p.arquivoSiteAtualizado}. Criando um arquivo inicial.`)
-        fs.writeFileSync(p.arquivoSiteAtualizado, novoSite);
+        await compare(page,
+          p.url,
+          config.notify,
+          p.bot_chatIds,
+          p.arquivoSiteAtualizado,
+          p.arquivoSiteAntesAtualizacao,
+          p.arquivoSiteAlteracoesScreenshot,
+          p.arquivoSiteAlteracoes,
+          p.verificarSomenteAreaSelecionada,
+          p.seletor);
       }
     }
-    //);
 
     setTimeout(async () => {
       await browser.close();
